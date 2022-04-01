df506d4faaa8fee0189b3fa1552e28f49fc9e675
tweneboahs <99669325+tweneboahs@users.noreply.github.com>
2022-04-01 10:08:07 -0400

    Update queues.js

--- queues.js
+++ queues.js
@@ -1,1 +1,95 @@
+//STANDARD QUEUE
+class Queue {
+    constructor(){
+        this.collection = [];
+    }
 
+    print(){
+        return this.collection
+    }
+
+    enqueue(value){
+        this.collection.push(value);
+    }
+
+    dequeue(){
+        this.collection.shift();
+    }
+
+    front(){
+        return this.collection[0];
+    }
+
+    size(){
+        return this.collection.length;
+    }
+
+    isEmpty(){
+        return (this.collection.length == 0);
+    }
+}
+
+// const myQueue = new Queue
+// myQueue.enqueue(6)
+// myQueue.enqueue(099)
+// myQueue.enqueue(6282)
+// myQueue.enqueue(8182)
+// myQueue.dequeue()
+// console.log(myQueue.size())
+// console.log(myQueue.front())
+// console.log(myQueue.isEmpty())
+
+
+//PRIORITY QUEUE
+class PriorityQueue {
+    constructor(){
+        this.collection = [];
+    }
+
+    print(){
+        return this.collection
+    }
+
+    enqueue(value){
+        if ( this.collection.length === 0){
+            this.collection.push(value);
+        } else {
+            let elementAdded = false;
+            for ( let i = 0; i < this.collection.length; i++){
+                if (value[1] < this.collection[i][1]){
+                    elementAdded = true;
+                    break;
+                }
+            }
+            if (elementAdded == false){
+                this.collection.push(value);
+            }
+        }
+        
+    }
+
+    dequeue(){
+        this.collection.shift();
+    }
+
+    front(){
+        return this.collection[0];
+    }
+
+    size(){
+        return this.collection.length;
+    }
+
+    isEmpty(){
+        return (this.collection.length == 0);
+    }
+}
+
+const myPQ = new PriorityQueue
+myPQ.enqueue(['kenke', 8])
+myPQ.enqueue(['armin', 4])
+myPQ.enqueue(['cole', 10])
+myPQ.enqueue(['sarah', 2])
+myPQ.enqueue(['harriet', 8])
+myPQ.enqueue(['ruth', 6])
+console.log(myPQ.print())
