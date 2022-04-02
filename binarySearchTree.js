9508cbe1dd31e9b6b28f6ce2dc80cb7f81300d35
tweneboahs <99669325+tweneboahs@users.noreply.github.com>
2022-04-02 14:44:13 -0400

    Update binarySearchTree.js

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -39,7 +39,7 @@ function depthFirstValues(root) {
     }
     return result;
 }
-console.log(depthFirstValuesRec(a));
+console.log(breadthFirstValues(a));
 
 //DEPTH FIRST VALUES
     //2. SOLVED RECURSIVELY
@@ -50,4 +50,72 @@ function depthFirstValuesRec(root){
     const leftValues = depthFirstValuesRec(root.left); //[b,d,e] - actually write what you expect the answer to be
     const rightValues = depthFirstValuesRec(root.right);//[c,f]
     return [root.val, ...leftValues, ...rightValues];
+}
+
+
+//BREADTH FIRST VALUES -  cant do recursively bc it doesnt involve a stack
+
+function breadthFirstValues(root){
+    if(root === null) return [];
+
+    const queue = [ root ];
+    const result = [];
+
+    while( queue.length > 0){
+        let current = queue.shift();
+        result.push(current.val);
+
+        current.left && queue.push(current.left);
+        current.right && queue.push(current.right);
+    }
+    return result;
+}
+
+function treeIncludesBF(root, target){
+    if(root === null) return false;
+
+    const queue = [ root ];
+
+    while( queue.length > 0){
+        let current = queue.shift();
+
+        if( current.val == target){
+            return true;
+            break;
+        } else {
+            current.left && queue.push(current.left);
+            current.right && queue.push(current.right);
+        }
+    }
+    return false;
+}
+
+console.log(treeIncludesRecursion(a,"c"));
+
+function treeIncludesRecursion(root, target){
+    if (root === null) return false;
+
+    if (root.val === target){
+        return true;
+    }
+    return treeIncludesRecursion(root.left, target) || treeIncludesRecursion(root.right, target);
+}
+
+function treeSumDepthFirst(root){
+    //remember to change the Nodes from letters to numbers if you want to calc properly
+    if ( root === null) return 0;
+
+    const stack = [ root ];
+    let sum = 0;
+
+    while (stack.length > 0){
+        let current = stack.pop();
+        sum += current.val;
+        current.left && stack.push(current.left);
+        current.right && stack.push(current.right);
+    }
+    
+    
+    return sum;
+    
 }
