0f4fe48d3d97447f7180dd1e08eeaa11b12434d8
tweneboahs <99669325+tweneboahs@users.noreply.github.com>
2022-04-14 20:29:10 -0400

    Update binarySearchTree.js

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -207,3 +207,48 @@ const pathFinderHelperRec2 = (root, target) => {
     
     return null;
 };
+
+
+//TREE VALUE COUNT
+
+//ITERATIVE
+
+const treeValueCount = (root, target) => {
+    if (root === null) return 0;
+    
+    let count = 0;
+    const stack = [ root ];
+    while (stack.length > 0) {
+      const current = stack.pop();
+      if (current.val === target) count += 1;
+      
+      if (current.left) stack.push(current.left);
+      if (current.right) stack.push(current.right);
+    }
+    
+    return count;
+};
+
+//RECURSIVE
+
+const treeValueCountRec = (root, target) => {
+    if (root === null) return 0;
+    const match = root.val === target ? 1 : 0;
+    return match + treeValueCountRec(root.left, target) + treeValueCountRec(root.right, target);
+};
+
+
+
+
+
+//HOW HIGH
+
+//RECURSIVE SOLUTION ONLY
+
+const howHigh = (node) => {
+    if (node === null) return -1;
+  
+    const leftHeight = howHigh(node.left);
+    const rightHeight = howHigh(node.right);
+    return 1 + Math.max(leftHeight, rightHeight);
+};
