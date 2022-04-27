5e9c06837830f903e329b9df76989c7392755da0
tweneboahs <99669325+tweneboahs@users.noreply.github.com>
2022-04-27 16:39:33 -0400

    Update dynamicProgramming.js

--- dynamicProgramming.js
+++ dynamicProgramming.js
@@ -8,3 +8,26 @@ const fib = (n, memo = {}) => {
     memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
     return memo[n];
 };
+
+
+
+
+//TRIBONACCI SEQUENCE
+
+const sumPossible = (amount, numbers, memo={}) => {
+    // todo
+    if ( amount === 0 ) return true;
+    if ( amount < 0 ) return false;
+    if ( amount in memo ) return memo[amount];
+    
+    
+    for ( number of numbers ){
+      if ( sumPossible( amount - number, numbers, memo ) ){
+        memo[amount] = true;
+        return true;
+      }
+    }
+    
+    memo[amount] = false;
+    return false;
+};
