9cbfd1ded76566a5fdb54c52c98f91b1b7bc22a3
tweneboahs <99669325+tweneboahs@users.noreply.github.com>
2022-05-05 18:06:21 -0400

    Update leetcode.js

--- leetcode.js
+++ leetcode.js
@@ -1,1 +1,42 @@
 
+//BINARY TREE
+
+
+
+//EASY
+
+//SAME TREE - ITERATIVE
+var isSameTree = function(p, q) {
+    
+    const stack1 = [p];
+    const stack2 = [q];
+    
+    while ( stack1.length && stack2.length ){
+        const curr1 = stack1.pop();
+        const curr2 = stack2.pop();
+        if ( curr1 === null || curr2 === null){
+            if ( curr1 !== curr2 ){
+                return false;
+            } else {
+                continue;
+            }
+        }
+        
+        if ( curr1.val !== curr2.val ){
+            return false;
+        }
+
+        
+        stack1.push(curr1.left);
+        stack1.push(curr1.right);
+        
+        stack2.push(curr2.left);
+        stack2.push(curr2.right);
+    }
+    
+    if (stack1.length || stack2.length) {
+        return false;
+    }
+    
+    return true;
+};
