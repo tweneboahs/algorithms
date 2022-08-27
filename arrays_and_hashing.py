557274df307926324464fc9343e8507c65a19a68
tweneboahs <tweneboahs@gmail.com>
2022-08-27 12:14:45 -0400

    Create new file arrays_and_hashing.py
    add TOP K FREQUENT ELEMENTS problem

---
+++ arrays_and_hashing.py
@@ -0,0 +1,22 @@
+# TOP K FREQUENT ELEMENTS
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            if num not in count:
+                count[num] = 0
+            count[num] += 1
+
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+# ________________________________________________________________
