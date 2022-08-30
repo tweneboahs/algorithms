2b8d3995dc3cab61b5d5b2eb2cf1a68741ea4d5c
tweneboahs <tweneboahs@gmail.com>
2022-08-30 14:18:23 -0400

    Add answer
    Add LONGEST CONSECUTIVE SEQUENCE problem to set

--- arrays_and_hashing.py
+++ arrays_and_hashing.py
@@ -20,3 +20,32 @@ class Solution:
                 if len(res) == k:
                     return res
 # ________________________________________________________________
+
+# LONGEST CONSECUTIVE SEQUENCE
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            # check if its the start of the sequence
+            if (n - 1) not in numSet:
+                length = 0
+                while (n + length) in numSet:
+                    length += 1
+
+                longest = max(longest, length)
+
+        return longest
+
+# ________________________________________________________________
+
+# ________________________________________________________________
+
+# ________________________________________________________________
+
+# ________________________________________________________________
+
+# ________________________________________________________________
